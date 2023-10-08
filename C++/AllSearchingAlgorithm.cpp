baad766b2c60554a7c02c2499cfea312216e1084
white-devil-01 <53394962+white-devil-01@users.noreply.github.com>
2023-10-08 20:30:58 +0530

    Update AllSearchingAlgorithm.cpp

--- C++/AllSearchingAlgorithm.cpp
+++ C++/AllSearchingAlgorithm.cpp
@@ -9,7 +9,7 @@ int linearSearch(const vector<int>& arr, int target) {
         if (arr[i] == target)
             return i;  // Return the index where the target is found
     }
-    return -1;  // Return -1 if the target is not found
+    return 0;
 }
 
 int binarySearch(const vector<int>& arr, int target) {
